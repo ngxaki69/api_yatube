94d4c8baeefdf4eeff3cb1bc4c4d736a9d64a7a8
ngxaki69 <makhnach.den@gmail.com>
2024-10-01 13:36:11 +0300

    Update import

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from posts.models import Post, Comment, Group
+from posts.models import Comment, Group, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, GroupViewSet
+from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
